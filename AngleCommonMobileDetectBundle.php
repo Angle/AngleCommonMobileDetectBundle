d21cfd8418c57a7a5d28f80982137f15d06ba3d3
Edmundo Fuentes <me@edmundofuentes.com>
2015-04-27 12:50:57 -0500

    Initial commit

---
+++ AngleCommonMobileDetectBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Angle\Common\MobileDetectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AngleCommonMobileDetectBundle extends Bundle
+{
+}
---
+++ DeviceDetector/MobileDetector.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Angle\Common\MobileDetectBundle\DeviceDetector;
+
+use Detection\MobileDetect;
+
+class MobileDetector extends MobileDetect
+{
+}
